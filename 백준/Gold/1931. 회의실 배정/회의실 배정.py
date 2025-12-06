ad72d3552517d0863e7d793625ba25e5fab0ceeb
bontak <142137769+kbt82883@users.noreply.github.com>
2025-12-07 01:50:20 +0900

    [Gold V] Title: 회의실 배정, Time: 2600 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,14 @@
 n = int(input())
-meetings = []
-
+meet = []
 for _ in range(n):
-    s, e = map(int, input().split())
-    meetings.append((s, e))
-    
-meetings.sort(key = lambda x: (x[1],x[0]))
+    s,e = map(int, input().split())
+    meet.append((s,e))
+
+meet.sort(key=lambda x: (x[1],x[0]))
 
 cnt = 0
 end_time = 0
-for s, e in meetings:
+for s,e in meet:
     if s >= end_time:
         cnt += 1
         end_time = e
