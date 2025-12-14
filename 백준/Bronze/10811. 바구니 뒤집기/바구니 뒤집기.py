355f2e9ee418f67847ce144b5646ef57bde804b5
bontak <142137769+kbt82883@users.noreply.github.com>
2025-12-15 00:42:36 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+arr = list(range(1, n + 1))
+
+for _ in range(m):
+    i, j = map(int, input().split())
+
+    while i < j:
+        arr[i - 1], arr[j - 1] = arr[j - 1], arr[i - 1]
+        i += 1
+        j -= 1
+print(*arr)
