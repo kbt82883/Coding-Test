29f2fd2bf2d3ad9539105fc814ee7964afd55811
bontak <142137769+kbt82883@users.noreply.github.com>
2025-12-15 22:24:50 +0900

    [Silver IV] Title: 다음 소수, Time: 1808 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for _ in range(int(input())):
+    n = int(input())
+    while True:
+        if is_prime(n):
+            print(n)
+            break
+        n += 1
