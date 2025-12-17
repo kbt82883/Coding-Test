136902ba5e6d45742f18605dd37918512e3313ca
bontak <142137769+kbt82883@users.noreply.github.com>
2025-12-17 21:21:02 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def bt(lst):
+    if len(lst) == M:
+        print(*lst)
+        return
+    for i in range(1, N + 1):
+        if lst and i < lst[-1]:
+            continue
+        lst.append(i)
+        bt(lst)
+        lst.pop()
+
+
+N, M = map(int, input().split())
+lst = []
+bt(lst)
