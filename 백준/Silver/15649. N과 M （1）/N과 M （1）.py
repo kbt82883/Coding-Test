3f786cbc9a9c86962f84c805cdf915022ed49cf9
bontak <142137769+kbt82883@users.noreply.github.com>
2025-12-17 21:11:33 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def bt(lst):
+    if len(lst) == M:
+        print(*lst)
+        return
+    for i in range(1, N + 1):
+        if i not in lst:
+            lst.append(i)
+            bt(lst)
+            lst.pop()
+
+
+N, M = map(int, input().split())
+lst = []
+bt(lst)
