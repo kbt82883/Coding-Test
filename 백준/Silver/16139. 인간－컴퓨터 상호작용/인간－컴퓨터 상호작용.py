7973b8868ee53cc3343231158553ea02e779a601
bontak <142137769+kbt82883@users.noreply.github.com>
2026-02-11 11:01:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 560 ms, Memory: 94412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,20 +1,19 @@
 import sys
 
 input = sys.stdin.readline
+print = sys.stdout.write
 
-S = input()
+S = input().strip()
 q = int(input())
 
+sumLst = [[0] * 26 for _ in range(len(S) + 1)]
+
+for i, ch in enumerate(S):
+    sumLst[i + 1] = sumLst[i][:]
+    sumLst[i + 1][ord(ch) - ord("a")] += 1
+
 for _ in range(q):
-    lst = input().split()
-    a = lst[0]
-    l, r = map(int, lst[1:3])
-
-    sum_lst = [0]
-    for x in range(len(S)):
-        if S[x] == a:
-            sum_lst.append(sum_lst[-1] + 1)
-        else:
-            sum_lst.append(sum_lst[-1])
-
-    print(sum_lst[r + 1] - sum_lst[l])
+    a, l, r = input().split()
+    l, r = int(l), int(r)
+    a = ord(a) - ord("a")
+    print(str(sumLst[r + 1][a] - sumLst[l][a]) + "\n")
